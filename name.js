d3598eb766a8ab4563fbccf432491152bf2790a5
snowdl <41115846+snowdl@users.noreply.github.com>
2018-07-11 21:35:38 +0900

    Add files via upload

---
+++ name.js
@@ -0,0 +1,20 @@
+
+var myPeople = [
+    {
+    "name" : "Jess",
+	"nationality" : "Korean",
+	"favColor" : "Black"},
+
+    {
+    "name" : "Mattew",
+	"nationality" : "Korean",
+	"favColor" : "Red"},
+
+    {
+    "name" : "Suji"
+	"nationality" : "Canedian"
+	"favColor" : "Orange"
+}
+]
+
+myPeople [1].favColor
